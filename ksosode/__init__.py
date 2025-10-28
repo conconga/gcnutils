77a44239cfbdb8b046f77f85f73e461240cd63a3
luciano kruk <sp04@kruk.eng.br>
2025-10-28 18:33:15 +0100

    extending the list of imported classes

--- ksosode/__init__.py
+++ ksosode/__init__.py
@@ -1,5 +1,5 @@
-from .kSosode import kSosode, kSosodeFunction
+from .kSosode           import kSosode, kSosodeFunction
+from .kSosodeIntegrator import kSosodeUtils, kSosodeIntegrator
 
 __version__ = "1.0.0"
 __author__  = "Luciano A. Kruk"
-
