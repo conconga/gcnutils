5653d819cc4c6ad8f788d47346cf1353c42be4b3
luciano kruk <sp04@kruk.eng.br>
2026-04-01 00:00:39 +0200

    tests for kArrayLib moved to it

--- tests/test_kArrayNav.py
+++ tests/test_kArrayNav.py
@@ -1,13 +1,11 @@
-import sys    
+#>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>--<<..>>
+import sys
+print( "**************************************" )
 print(f"** __name__    = {__name__}")
 print(f"** __package__ = {__package__}")
 print(f"** sys.path[0] = {sys.path[0]}")
 
-# when this is called from within the folder 'tests/', we
-# need to add the path to the package __init__.py:
-sys.path.append("..")
-
-from knavigation import *
+from knavigation import kArrayNav, kArray
 import numpy as np
 from numpy import pi
 from math import sqrt
@@ -15,56 +13,6 @@ import math
 
 
 class TestClass_kArrayNav:
-
-    def test_to_skew(self):
-        print("==== to_skew() ====")
-        a = kArrayNav( [1,2,3], hvector=False )
-        b = [0,-3,2,3,0,-1,-2,1,0]
-        assert all([i==j for i,j in zip(a.to_skew(),b)])
-        print(a.to_skew())
-
-    def test_cross_product(self):
-        print("==== cross product ====")
-        a = kArrayNav( [5,-2,4], hvector=True )
-        b = kArrayNav( [-1,2,3], hvector=True )
-
-        try:
-            ok = False
-            c  = a.X(b)
-        except:
-            ok = True
-        if not ok:
-            raise(NameError("should not get here!"))
-
-        c1 = a.T.X(b.T)
-        c2 = np.cross(a.squeeze(), b.squeeze())
-        c3 = a.to_skew() * b.T
-
-        for i,j,k in zip(c1,c2,c3):
-            assert abs(i-j) < 1e-10
-            assert abs(i-k) < 1e-10
-
-    def test_vector_norm(self):
-        print("==== norm ====")
-        a = kArrayNav([1,1])
-        assert abs(a.norm() - math.sqrt(2)) < 1e-10
-        print("||a|| = {:f}".format(abs(a)))
-        a = kArrayNav(-32)
-        assert abs(abs(a) - 32.0) < 1e-10
-
-    def test_matrix_inv(self):
-        print("==== inv() ====")
-        M = [[1,2,3], [40,5,6], [7,8,9]]
-        #M = np.random.randn(3,3)
-        a = kArrayNav(M)
-        b = kArrayNav(M)
-        identity = a.inv() * b
-        for i in range(3):
-            for j in range(3):
-                if i == j:
-                    assert abs(identity[i,j] - 1.0) < 1e-10
-                else:
-                    assert abs(identity[i,j]) < 1e-10
 
     def test_to_rad_to_deg(self):
         print("==== to_rad, to_deg ====")
@@ -200,13 +148,6 @@ class TestClass_kArrayNav:
             ax.plot(np.linspace(0,3000,20), g)
 
         ax.legend(leg)
-
-        # apply(fn)
-        print("==== apply() ====")
-        a = kArrayNav( [[2,0,0],[0,4,0],[0,0,10]] )
-        b = a.apply(lambda x: np.linalg.inv(x))
-        for i in range(3):
-            assert b[i,i] == 1./a[i,i]
 
         #----------------------#
         # coherence tests
