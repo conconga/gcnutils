b174dcdc84461ad8768d6eb43993f78d7b7c02f4
luciano kruk <sp04@kruk.eng.br>
2026-06-07 19:05:16 +0200

    tests in the folder test

--- kltisystems/__init__.py
+++ kltisystems/__init__.py
@@ -3,7 +3,7 @@ from .k1orderltisysmimo     import k1OrderLTIsysMimoDiscrete, fn_example_mimo
 from .k1orderltisyssiso     import fn_example_siso
 
 from .k2orderltisyssiso     import k2OrderLTIsysSisoFactory
-from .k2orderltisysmimo     import k2OrderLTIsysMimo, k2OrderLTIsysMimoTests
+from .k2orderltisysmimo     import k2OrderLTIsysMimo
 from .kNOrderDerivativeSiso import kNOrderDerivativeSiso, kNOrderDerivativeSisoTests
 
 __version__ = "1.0.0"
